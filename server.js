1f7578e4440d56cee62e23720f611270eeadb69a
Vinothini18 <Vinothini18>
2020-06-21 18:16:54 +0530

    added General Styles

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
+ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
   
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
   const bodyParser = require('body-parser')
+  const methodOverride = require('method-override')
   
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
@@ -15,12 +16,14 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
+  app.use(methodOverride('_method'))
   app.use(express.static('public'))
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
   const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true ,
-    useUnifiedTopology: true})
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,
+    useUnifiedTopology: true
+   })
   const db = mongoose.connection
   db.on('error', error => console.error(error))
   db.once('open', () => console.log('Connected to Mongoose'))
@@ -30,8 +33,6 @@ if (process.env.NODE_ENV !== 'production') {
   app.use('/books', bookRouter)
   
   app.listen(process.env.PORT || 3000)
-
-
 
 
 
